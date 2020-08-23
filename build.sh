81dedf37355804f4c702c6beee4bd0f1ef0580c9
Dymerz <corentin.urbain96@gmail.com>
2020-08-23 15:57:44 +0200

    Improve build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python3 setup.py sdist bdist_wheel
-python3 -m pip install dist/swingingpy-0.0.1-py3-none-any.whl
+python3 -m pip uninstall --yes dist/swingingpy-0.0.1-py3-none-any.whl
+yes | python3 -m pip install  dist/swingingpy-0.0.1-py3-none-any.whl
 
 exit 0
