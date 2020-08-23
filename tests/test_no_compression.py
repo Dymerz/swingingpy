6321ac77ea392f8c26b1f268f845c9c5cd9e8e3f
Dymerz <corentin.urbain96@gmail.com>
2020-08-23 15:57:22 +0200

    Improve tests

--- tests/test_no_compression.py
+++ tests/test_no_compression.py
@@ -3,14 +3,17 @@ import json
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-def test_import():
-    import swingingpy
-
 def test_simple():
     from swingingpy.compressor import SwingingDoor, CPoint
 
-    comp_dev = 1
-    comp_max = 5
+    expected_points = [6, 8, 10, 14, 16, 19]
+    points = []
+    with open(dir_path + "/points.json") as _f:
+        for _j in json.loads(_f.read()):
+            points.append(CPoint(_j[0], _j[1]))
+
+    comp_dev = 0
+    comp_max = -1
     original = CPoint(0, 0)
     snapshot = None
 
@@ -21,12 +24,6 @@ def test_simple():
         snapshot=snapshot
     )
 
-    expected_points = [5, 9, 13, 16, 19]
-    points = []
-    with open(dir_path + "/points.json") as _f:
-        for _j in json.loads(_f.read()):
-            points.append(CPoint(_j[0], _j[1]))
-
     result_points = []
     for _p in points:
         point = swing.check(_p)
@@ -34,4 +31,4 @@ def test_simple():
             result_points.append(point.get_x())
 
     assert result_points == expected_points
-    assert swing.get_compression_ratio() == 75
+    assert swing.get_compression_ratio() == 70
